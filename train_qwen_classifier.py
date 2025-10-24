ad14410437fd77b55c24c2b72fdbb7c6d54c7da4
Giles Thomas <giles@giles.net>
2025-10-24 21:58:47 +0100

    Built out training loop

--- train_qwen_classifier.py
+++ train_qwen_classifier.py
@@ -1,5 +1,10 @@
+import datetime
+import time
+from pathlib import Path
+
 import pandas as pd
 import torch
+from safetensors.torch import save_file
 
 from torch.utils.data import DataLoader, Dataset
 from transformers import AutoModelForCausalLM, AutoTokenizer
@@ -102,7 +107,6 @@ def load_datasets(tokenizer):
     return train_loader, val_loader, test_loader
 
 
-
 def calc_accuracy_loader(data_loader, model, num_batches=None):
     device = next(model.parameters()).device
     model.eval()
@@ -132,6 +136,109 @@ def calc_accuracy_loader(data_loader, model, num_batches=None):
     return correct_predictions / num_examples
 
 
+def calc_loss_batch(input_batch, target_batch, model):
+    device = next(model.parameters()).device
+    input_batch = input_batch.to(device)
+    target_batch = target_batch.to(device)
+    logits = model(input_batch).logits[:, -1, :]
+    return torch.nn.functional.cross_entropy(logits, target_batch)
+
+
+def calc_loss_loader(data_loader, model, num_batches=None):
+    total_loss = 0
+    if len(data_loader) == 0:
+        return float("nan")
+    if num_batches is None:
+        num_batches = len(data_loader)
+    else:
+        num_batches = min(num_batches, len(data_loader))
+    for i, (input_batch, target_batch) in enumerate(data_loader):
+        if i >= num_batches:
+            break
+        loss = calc_loss_batch(input_batch, target_batch, model)
+        total_loss += loss.item()
+    return total_loss / num_batches
+
+
+def evaluate_model(model, train_loader, val_loader, eval_iter):
+    model.eval()
+    with torch.no_grad():
+        train_loss = calc_loss_loader(train_loader, model, num_batches=eval_iter)
+        val_loss = calc_loss_loader(val_loader, model, num_batches=eval_iter)
+
+    model.train()
+    return train_loss, val_loss
+
+
+
+def save_checkpoint(model):
+    checkpoints_dir = Path(__file__).resolve().parent / "checkpoints"
+    if checkpoints_dir.exists():
+        assert checkpoints_dir.is_dir()
+    else:
+        checkpoints_dir.mkdir()
+
+    now = datetime.datetime.now(datetime.UTC)
+    checkpoint_dir = checkpoints_dir / f"{now:%Y%m%dZ%H%M%S}"
+    checkpoint_dir.mkdir()
+
+    tensors_file = checkpoint_dir / "model.safetensors"
+    save_file(model.state_dict(), tensors_file)
+
+    skdljfsldkfj
+
+
+def train_classifier_simple(
+    model, train_loader, val_loader, optimizer,
+    num_epochs, eval_freq, eval_iter
+):
+    train_losses = []
+    val_losses = []
+    train_accs = []
+    val_accs = []
+
+    examples_seen = 0
+    global_step = -1
+
+    for epoch in range(num_epochs):
+        model.train()
+
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad()
+            loss = calc_loss_batch(input_batch, target_batch, model)
+            loss.backward()
+            optimizer.step()
+
+            examples_seen += input_batch.shape[0]
+            global_step += 1
+
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(
+                    model, train_loader, val_loader, eval_iter
+                )
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                print(
+                    f"Ep {epoch + 1} (Step {global_step:06d}): "
+                    f"Train loss {train_loss:.3f} "
+                    f"Val loss {val_loss:.3f}"
+                )
+                save_checkpoint(model)
+
+        train_accuracy = calc_accuracy_loader(
+            train_loader, model, num_batches=eval_iter
+        )
+        val_accuracy = calc_accuracy_loader(
+            val_loader, model, num_batches=eval_iter
+        )
+
+        print(f"Training accuracy: {train_accuracy * 100:.2f}%")
+        print(f"Validation accuracy: {val_accuracy * 100:.2f}%")
+        train_accs.append(train_accuracy)
+        val_accs.append(val_accuracy)
+
+    return train_losses, val_losses, train_accs, val_accs, examples_seen
+
 
 def train(model, train_loader, val_loader, test_loader):
     train_accuracy = calc_accuracy_loader(
@@ -147,6 +254,28 @@ def train(model, train_loader, val_loader, test_loader):
     print(f"Training accuracy: {train_accuracy * 100:.2f}%")
     print(f"Validation accuracy: {val_accuracy * 100:.2f}%")
     print(f"Test accuracy: {test_accuracy * 100:.2f}%")
+
+    with torch.no_grad():
+        train_loss = calc_loss_loader(train_loader, model, num_batches=5)
+        val_loss = calc_loss_loader(val_loader, model, num_batches=5)
+        test_loss = calc_loss_loader(test_loader, model, num_batches=5)
+    print(f"Training loss: {train_loss:.3f}")
+    print(f"Validation loss: {val_loss:.3f}")
+    print(f"Test loss: {test_loss:.3f}")
+
+    start_time = time.time()
+    torch.manual_seed(123)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5, weight_decay=0.1)
+    num_epochs = 5
+
+    train_classifier_simple(
+        model, train_loader, val_loader, optimizer,
+        num_epochs=num_epochs, eval_freq=50, eval_iter=5
+    )
+
+    end_time = time.time()
+    execution_time_minutes = (end_time - start_time) / 60
+    print(f"Training completed in {execution_time_minutes:.2f} minutes.")
 
 
 
