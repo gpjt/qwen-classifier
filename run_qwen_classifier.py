a1056c7ae48ac588b078f331b26d2a9c02bda0b0
Giles Thomas <giles@giles.net>
2025-10-24 22:38:26 +0100

    Tidying, note to future self

--- run_qwen_classifier.py
+++ run_qwen_classifier.py
@@ -14,7 +14,10 @@ def main(input_text, checkpoint):
     input_tokens = tokenizer.encode(input_text)
     input_batch = torch.tensor(input_tokens).unsqueeze(0)
     input_batch = input_batch.to(next(model.parameters()).device)
+
+    # Zero'th batch, last token logits
     output_logits = model(input_batch).logits[0, -1, :]
+
     output_probs = torch.nn.functional.softmax(output_logits, dim=-1)
     ham_prob, spam_prob = output_probs.tolist()
     print(f"Ham: {ham_prob * 100:.2f} || Spam: {spam_prob * 100:.2f}")
